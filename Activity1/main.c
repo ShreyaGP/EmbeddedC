f157113b6cc676087bcb6eb816937fc4db8a16ba
ShreyaGP <you@example.com>
2021-04-23 20:30:07 +0530

    Updated main

--- Activity1/main.c
+++ Activity1/main.c
@@ -2,24 +2,21 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define LED (PORTB1)
-#define SEAT (PORTB0)
-
-unsigned volatile HEATER =0;
+unsigned volatile FLAG =0;
 
 int main(void)
 {
     port();
     sei();
     while(1){
-        if((PINB & (1<<SEAT))){
-        if(HEATER==1){
-            PORTB  |= (1<<LED);
+        if((PINB & (1<<PB0))){
+        if(FLAG==1){
+            PORTB  |= (1<<PB1);
             _delay_ms(1000);
-            HEATER=0;
+            FLAG=0;
             }
         else{
-            PORTB &= ~(1<<LED);
+            PORTB &= ~(1<<PB1);
             _delay_ms(100);
         }
         }
@@ -29,5 +26,5 @@ int main(void)
 
 ISR(INT0_vect)
 {
-    HEATER = 1;
+    FLAG = 1;
 }
--- Activity1/port.c
+++ Activity1/port.c
@@ -3,11 +3,11 @@
 
 void port()
 {
-    DDRB |= (1<<PB1);  //B1 IS SET  AS OUTPUT
-    DDRB &= ~(1<<PB0);  //B0 is cleared to set as input
+    DDRB |= (1<<PB1);  //B1 IS SET  AS OUTPUT FOR LED
+    DDRB &= ~(1<<PB0);  //B0 is cleared to set as input for SEAT
 
     DDRD &= ~(1<<PD2); //clear bit
-    PORTD  &= ~(1<<PD2);
+    PORTD  &= ~(1<<PD2);  //HEATER interrupt
 
     EICRA |= (1<<ISC00);  //external interrupt control register
     EIMSK |= (1<<INT0);  //ext interrupt mask register set for INT0
